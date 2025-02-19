4bd670bb23f5801442122d4aad880d2c1ff36904
chandukrosuri <srichandan939@gmail.com>
2025-02-19 13:44:00 -0500

    basic http routing, gitignore, environment variables, nodemon

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 import http from 'http';
 
-const server = http.createServer((req, res) => {
-    // res.setHeader('Content-Type','text/html');
-    // res.write('<h1>server is up<h1>');
-
-    res.writeHead(500, {'Content-Type': 'application/json'})
-
-    res.end(JSON.stringify({message: 'Server not found'}));
+const server = http.createServer((req, res) => { 
+    try {
+        if (req.method === 'GET') {
+            if (req.url === '/') {
+                res.writeHead(200, {'content-Type': 'text/html'});
+                res.end('<h1>Home page</h1>');
+            } else if (req.url === '/about') {
+                res.writeHead(200, {'content-Type': 'text/html'});
+                res.end('<h1>About page</h1>');
+            } else {
+                res.writeHead(404, {'content-Type': 'text/JSON'});
+                res.end('{message: invalid page}');
+            }
+        }
+    } catch (error) {
+        
+    }
 });
 
-const port = 8000;
+const port = process.env.PORT;
 
 server.listen(port, () => {
     console.log(`connected on port ${port}`);
